41bb7da241ca492cf388f200fb6b8783a82583c6
BECOMEME <terrylennon66@outlook.com>
2025-01-02 19:31:37 +0100

    connect db

--- books/src/main/java/dev/stone66/books/BooksApplication.java
+++ books/src/main/java/dev/stone66/books/BooksApplication.java
@@ -3,11 +3,12 @@ package dev.stone66.books;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+//@RestController
 public class BooksApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BooksApplication.class, args);
-	}
+		SpringApplication.run(BooksApplication.class, args);}
 
 }
